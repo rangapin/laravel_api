09bb3dff0142fe2bd9f3d91a963df91fb7aa355b
rangapin <rangapin@gmx.com>
2022-12-23 18:51:38 -0600

    initial commit

---
+++ app/Models/Author.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Author extends Model
+{
+    protected $table = 'authors';
+
+    protected $fillable = [
+        'name',
+        'title',
+        'company',
+        'email'
+    ];
+
+    public function books() {
+        return $this ->hasMany(Book::class);
+    }
+}
